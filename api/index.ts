dca1a5dfd6d2bfec7c3623d210a98c1977ed8113
Cherish.the.creator <lytlesolutionsllc@gmail.com>
2026-04-15 18:58:39 -0700

    fix: update api/index.ts to import from compiled dist

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,2 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-import app from '../src/index';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  return app(req, res);
-}
+const app = require('../dist/index').default;
+module.exports = app;
